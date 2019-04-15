bb570304fba87c0345115fbcfc75c3e39592d933
xiaohaibiao <xiaohaibiao331@outlook.com>
2019-04-15 21:17:48 +0800

    update makefile

--- build/source/subdir_rules.mk
+++ build/source/subdir_rules.mk
@@ -11,6 +11,6 @@ source/%.o: ../source/%.s $(GEN_OPTS) | $(GEN_FILES)
 source/%.o: ../source/%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: GNU Compiler'
-	$(ARM-CC) -c -mfpu=vfpv3-d16 -I"/root/tms570" -I"/root/include" -g3 -gdwarf-3 -w -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	$(ARM-CC) -c -mfpu=vfpv3-d16 -I"/root/tms570" -I"/root/tms570/include" -g3 -gdwarf-3 -w -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
